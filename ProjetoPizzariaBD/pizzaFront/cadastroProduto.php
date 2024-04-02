f6c46d672f962de0b0532ced184248a076bf9303
ryangabriel27 <ryanjulio56@gmail.com>
2024-04-02 16:54:02 -0300

    StyleCorrigido

--- ProjetoPizzariaBD/pizzaFront/cadastroProduto.php
+++ ProjetoPizzariaBD/pizzaFront/cadastroProduto.php
@@ -1,5 +1,5 @@
-<?php 
-    include 'functions.php';
+<?php
+include 'functions.php';
 ?>
 
 <!DOCTYPE html>
@@ -10,29 +10,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pizzaria</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styleP.css">
 </head>
 
 <body>
-    <?=header2()?>
+    <?= header2() ?>
     <section class="main-cadastro">
-        <div class="container-cadastro">
-            <div class="img-cadastro">
-                <img src="img/logoSemFundo2.png" alt="">
-            </div>
+        <div class="content update">
             <form action="cadastro_prod.php" method="POST">
                 <label for="cNome">Digite o nome do produto:</label>
-                <input type="text" name="cNome" id="cNome" placeholder="Digite seu nome" /><br />
                 <label for="cValor">Digite o valor do produto:</label>
+                <input type="text" name="cNome" id="cNome" placeholder="Digite seu nome" /><br />
                 <input type="text" name="cValor" id="cValor" placeholder="Digite o valor" /><br>
                 <label for="cCategoria">Digite a categoria do produto:</label>
-                <input type="text" name="cCategoria" id="cCategoria" placeholder="Digite a categoria" /><br>
                 <label for="cIngr">Digite os ingredientes do produto:</label>
+                <input type="text" name="cCategoria" id="cCategoria" placeholder="Digite a categoria" /><br>
                 <input type="text" name="cIngr" id="cIngr" placeholder="Digite os principais ingredientes" /><br>
                 <label for="cTamanho">Digite o tamanho do produto:</label>
-                <input type="text" name="cTamanho" id="cTamanho" placeholder="Digite o tamanho" /><br>
                 <label for="cBorda">Digite a borda do produto:</label>
+                <input type="text" name="cTamanho" id="cTamanho" placeholder="Digite o tamanho" /><br>
                 <input type="text" name="cBorda" id="cBorda" placeholder="Digite a borda" /><br>
-                <button type="submit" id="enviarDados">Cadastrar</button>
+                <input type="submit" id="enviarDados"></input>
             </form>
         </div>
     </section>
